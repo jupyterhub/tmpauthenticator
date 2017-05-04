b7aa5469c44bb51c6fe59f0428ce604120e056f9
yuvipanda <yuvipanda@gmail.com>
2017-05-04 14:33:13 -0700

    Add feature to force a new single-user server each time
    Gives users a new single-user server each time they hit the login
    URL, rather than redirecting them to the current instance they
    are already on

--- tmpauthenticator/__init__.py
+++ tmpauthenticator/__init__.py
@@ -1,5 +1,6 @@
 import uuid
 
+from traitlets import Bool
 
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
@@ -12,7 +13,13 @@ class TmpAuthenticateHandler(BaseHandler):
 
     Creates a new user with a random UUID, and auto starts their server
     """
+    def initialize(self, force_new_server):
+        super().initialize()
+        self.force_new_server = force_new_server
+
     def get(self):
+        if self.force_new_server:
+            self.clear_login_cookie()
         username = str(uuid.uuid4())
         user = self.user_from_username(username)
         self.set_login_cookie(user)
@@ -30,9 +37,25 @@ class TmpAuthenticator(Authenticator):
     page immediately logs the user in with a randomly generated UUID if they
     are already not logged in, and spawns a server for them.
     """
+    force_new_server = Bool(
+        False,
+        help="""
+        Automatically log out the user first before logging them in.
+
+        When set to True, users going to /hub/login will *always* get a
+        new single-user server. When set to False, they'll just be
+        redirected to their current session if one exists.
+        """,
+        config=True
+    )
+
     def get_handlers(self, app):
+        # FIXME: How to do this better?
+        extra_settings = {
+            'force_new_server': self.force_new_server
+        }
         return [
-            ('/tmplogin', TmpAuthenticateHandler)
+            ('/tmplogin', TmpAuthenticateHandler, extra_settings)
         ]
 
     def login_url(self, base_url):
