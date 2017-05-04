8231fdad0899480df7d4916bf6150c86d64be67a
yuvipanda <yuvipanda@gmail.com>
2017-05-04 15:28:25 -0700

    Add ability for subclasses to modify created user before spawning
    This is primarily used right now to allow the image to be spawned
    to be specified as a query param, but is generic enough for future
    unforeseen uses

--- tmpauthenticator/__init__.py
+++ tmpauthenticator/__init__.py
@@ -1,6 +1,7 @@
 import uuid
 
 from traitlets import Bool
+from tornado import gen
 
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
@@ -13,15 +14,18 @@ class TmpAuthenticateHandler(BaseHandler):
 
     Creates a new user with a random UUID, and auto starts their server
     """
-    def initialize(self, force_new_server):
+    def initialize(self, force_new_server, process_user):
         super().initialize()
         self.force_new_server = force_new_server
+        self.process_user = process_user
 
+    @gen.coroutine
     def get(self):
         if self.force_new_server:
             self.clear_login_cookie()
         username = str(uuid.uuid4())
-        user = self.user_from_username(username)
+        user_unprocessed = self.user_from_username(username)
+        user = yield gen.maybe_future(self.process_user(user_unprocessed, self))
         self.set_login_cookie(user)
         self.redirect(url_path_join(
             self.hub.server.base_url,
@@ -49,10 +53,25 @@ class TmpAuthenticator(Authenticator):
         config=True
     )
 
+    def process_user(self, user, handler):
+        """
+        Do additional arbitrary things to the created user before spawn.
+
+        user is a user object, and handler is a TmpAuthenticateHandler object
+
+        Should return the new user object.
+
+        This method can be a @tornado.gen.coroutine.
+
+        Note: This is primarily for overriding in subclasses
+        """
+        return user
+
     def get_handlers(self, app):
         # FIXME: How to do this better?
         extra_settings = {
-            'force_new_server': self.force_new_server
+            'force_new_server': self.force_new_server,
+            'process_user': self.process_user
         }
         return [
             ('/tmplogin', TmpAuthenticateHandler, extra_settings)
