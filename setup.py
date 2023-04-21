f2f6deed9962223a7386b53d2ae5c98844195f06
Erik Sundell <erik.i.sundell@gmail.com>
2023-04-21 03:06:22 +0200

    Register tmp authenticator with JupyterHub via entry_points

--- setup.py
+++ setup.py
@@ -8,5 +8,16 @@ setup(
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
     license='3 Clause BSD',
+    entry_points={
+        # Thanks to this, user are able to do:
+        #
+        #     c.JupyterHub.authenticator_class = "tmp"
+        #
+        # ref: https://jupyterhub.readthedocs.io/en/4.0.0/reference/authenticators.html#registering-custom-authenticators-via-entry-points
+        #
+        'jupyterhub.authenticators': [
+            'tmp = tmpauthenticator:TmpAuthenticator',
+        ],
+    },
     packages=find_packages(),
 )
