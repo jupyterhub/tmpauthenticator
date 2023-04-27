2fa0281f6ce39f38725151f8c121c5bcd1eadc47
Erik Sundell <erik.i.sundell@gmail.com>
2023-04-27 21:25:41 +0200

    Set authenticator_class using registered "tmp" handle

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -4,9 +4,7 @@ sample jupyterhub config file for testing tmpauthenticator
 
 c = get_config()  # noqa
 
-from tmpauthenticator import TmpAuthenticator
-
-c.JupyterHub.authenticator_class = TmpAuthenticator
+c.JupyterHub.authenticator_class = "tmp"
 
 from jupyterhub.spawner import SimpleLocalProcessSpawner
 
