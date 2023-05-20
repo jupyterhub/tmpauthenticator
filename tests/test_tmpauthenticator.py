7a85ffe90bf1a2ebf879c829041e2b8ecaeaf7a5
Erik Sundell <erik.i.sundell@gmail.com>
2023-05-20 15:52:34 +0200

    Add test of auto_login config

--- tests/test_tmpauthenticator.py
+++ tests/test_tmpauthenticator.py
@@ -7,3 +7,42 @@ Test ideas:
 - Validate that TmpAuthenticateHandler.get() redirects to something sensible.
 - Validate that a configured post_auth_hook is executed.
 """
+import pytest
+from yarl import URL
+
+
+@pytest.mark.parametrize(
+    "test_config, test_status, test_location, test_url",
+    [
+        ({}, 302, "hub/tmplogin", None),
+        ({"TmpAuthenticator": {"auto_login": True}}, 302, "hub/tmplogin", None),
+        ({"TmpAuthenticator": {"auto_login": False}}, 200, None, "hub/login"),
+    ],
+)
+async def test_auto_login_config(
+    hub_app,
+    hub_config,
+    browser_session,
+    test_config,
+    test_status,
+    test_location,
+    test_url,
+):
+    """
+    Tests TmpAuthenticator.auto_login's behavior when its default value is used,
+    and when its explicitly set.
+    """
+    hub_config.merge(test_config)
+
+    app = await hub_app(hub_config)
+    app_port = URL(app.bind_url).port
+    app_url = URL(f"http://localhost:{app_port}{app.base_url}")
+
+    login_url = str(app_url / "hub/login")
+    r = await browser_session.get(login_url, allow_redirects=False)
+
+    assert r.status == test_status
+    if test_url:
+        assert test_url in str(r.url)
+    if test_location:
+        assert test_location in r.headers["Location"]
