6d9237f524c8a21dce50116eb7c5a9d5b6aa5b89
Erik Sundell <erik.i.sundell@gmail.com>
2023-05-20 16:20:25 +0200

    Add test of post_auth_hook

--- tests/test_tmpauthenticator.py
+++ tests/test_tmpauthenticator.py
@@ -3,7 +3,6 @@ Test ideas:
 
 - Validate that TmpAuthenticateHandler.get() creates a new user and updates the
   login cookie even if it was already set.
-- Validate that a configured post_auth_hook is executed.
 """
 import pytest
 from yarl import URL
@@ -62,3 +61,40 @@ async def test_login(
     r = await browser_session.get(home_url)
 
     assert r.status == 200
+
+
+@pytest.mark.parametrize(
+    "test_setting_admin_to, test_status",
+    [
+        (True, 200),
+        (False, 403),
+    ],
+)
+async def test_post_auth_hook_config(
+    hub_app,
+    hub_config,
+    browser_session,
+    test_setting_admin_to,
+    test_status,
+):
+    """
+    Tests that the inherited Authenticator.post_auth_hook is respected by
+    updating the authentication dictionary's admin key and accessing /hub/admin,
+    which should result in a forbidden response if not configured as admin via
+    the post_auth_hook.
+    """
+
+    def set_admin_post_auth_hook(authenticator, handler, authentication):
+        authentication["admin"] = test_setting_admin_to
+        return authentication
+
+    hub_config.TmpAuthenticator.post_auth_hook = set_admin_post_auth_hook
+
+    app = await hub_app(hub_config)
+    app_port = URL(app.bind_url).port
+    app_url = URL(f"http://localhost:{app_port}{app.base_url}")
+
+    admin_url = str(app_url / "hub/admin")
+    r = await browser_session.get(admin_url)
+
+    assert r.status == test_status
